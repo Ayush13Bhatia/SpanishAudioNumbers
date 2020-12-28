1bcf115275e455822d4f4df199488da0beb04835
Ayush13Bhatia <ayushbhatia668@gmail.com>
2020-12-28 23:14:22 +0530

    first commit

---
+++ lib/NumberAudio.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class NumberAudio {
+  String auidoUri;
+  MaterialColor buttonColor;
+  String buttonText;
+
+  NumberAudio(String aU, MaterialColor bC, String bT) {
+    this.auidoUri = aU;
+    this.buttonColor = bC;
+    this.buttonText = bT;
+  }
+}
